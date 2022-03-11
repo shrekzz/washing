ce51a5c3e9536975e5adbb174c4ad280e7987d79
谢梽 <553238045@qq.com>
2022-03-11 09:16:54 +0800

    ver0.1.6

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,10 +1,11 @@
 import log from 'electron-log'
 log.transports.file.level = 'debug'
 log.transports.file.maxSize = 1002430 // 10M
+log.transports.console.level = 'silly'
 log.transports.file.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}]{scope} {text}'
 let date = new Date()
 date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-log.transports.file.resolvePath = () => './' + date + '.log'
+log.transports.file.resolvePath = () => 'D:\\WASHING_WORK\\' + date + '.log'
 const logger = {
   info (param) {
     log.info(param)
